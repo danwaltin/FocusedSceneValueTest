7173bee9f397b695aa4529beb311bbba754065d2
Dan Waltin <dan@waltin.se>
2021-10-31 09:42:56 +0100

    adding xcode project

---
+++ FocusedSceneValueTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FocusedSceneValueTest
+//
+//  Created by Dan Waltin on 2021-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FocusedSceneValueTest/FocusedSceneValueTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusedSceneValueTestApp.swift
+//  FocusedSceneValueTest
+//
+//  Created by Dan Waltin on 2021-10-31.
+//
+
+import SwiftUI
+
+@main
+struct FocusedSceneValueTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
